9a0e5064c856217336e9b4c833e4e92165a3e454
unicornautomata <blaslomibao.com>
2025-07-20 21:31:35 +0800

    Upload Change Remove logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Navbar.css'; // or create Header.css if you prefer separate styling
 import './Header.css';
-import logo from '../assets/logo.png'; // or './logo.png' if in same folder
+//import logo from '../assets/logo.png'; // or './logo.png' if in same folder
 //<img src={logo} alt="App Logo" className="app-logo" />
 function Header() {
   return (
